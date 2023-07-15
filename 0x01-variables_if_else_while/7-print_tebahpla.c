867f25e5b6cde3f81bdd40f0c29d01c02ca47a16
martmaro <odhisluka@gmail.com>
2023-07-15 09:38:30 -0700

    7-pritn_tebaphla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - starts the program
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+		while
+			(c >= 'a') {
+				putchar(c);
+				c--;
+			}
+	putchar('\n');
+	return(0);
+}
