467eff22d6545dc9d321e5d67afd619e8c05e656
martmaro <odhisluka@gmail.com>
2023-08-04 17:29:52 -0700

    0x0A. C - argc, argv

---
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of the program
+ * @argc: number of arguments
+ * argv: array of arguments
+ * Return: Always 0
+ */
+int main(int argc _attribute_((unused)), char *argve[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
